a7789e49f5a780a221b3f24b985d22b421aa3348
DavideFarci <yank92.df@gmail.com>
2025-04-27 15:23:50 +0200

    aggiunto piania per gestione store

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,10 @@
 import { createApp } from "vue";
 import "./style.css";
 import App from "./App.vue";
-import router from "./router/index.ts";
 
-createApp(App).use(router).mount("#app");
+import router from "./router/index";
+import { createPinia } from "pinia";
+
+const pinia = createPinia();
+
+createApp(App).use(router).use(pinia).mount("#app");
